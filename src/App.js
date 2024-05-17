038f5909a38caeb77eb7e2ccb5ebaa162a273809
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-05-17 21:18:56 +0530

    app rounting is updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ArchivedClasses, Calender, Home, Navbar, NotFound, RightNavbar, Settings } from './components/index.js';
 
-function App() {
+const App = () => {
+  
+  const [menuOpen, setMenuOpen] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar setMenuOpen={setMenuOpen} menuOpen={menuOpen} />
+      <div className='flex pt-16'>
+        <div className={`${menuOpen ? "w-1/4" : "w-[72px]"}`}>
+          <RightNavbar setMenuOpen={setMenuOpen} menuOpen={menuOpen} />
+        </div>
+        <div className={`${menuOpen ? "w-full -ml-2" : "ml-24"} pb-5`}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/calender' element={<Calender />} />
+            <Route path='/archived-classes' element={<ArchivedClasses />} />
+            <Route path='/settings' element={<Settings />} />
+            <Route path='/class/:id' element={<h1>Class</h1>} />
+            <Route path='/assignment/:id' element={<h1>Assignment</h1>} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
