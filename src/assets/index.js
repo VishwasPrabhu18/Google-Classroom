b4bfda46cac11092af28ebe1305332695b3642a6
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-05-17 21:16:47 +0530

    assets are added

---
+++ src/assets/index.js
@@ -0,0 +1,23 @@
+export { default as ClassroomLogo } from "./classroom.svg";
+export { default as Profile } from "./profile.png";
+
+export { default as Class1 } from "./class1.png";
+export { default as Class2 } from "./class2.png";
+export { default as Class3 } from "./class3.png";
+export { default as Class4 } from "./class4.png";
+export { default as Class5 } from "./class5.png";
+export { default as Class6 } from "./class6.png";
+export { default as Class7 } from "./class7.png";
+export { default as Class8 } from "./class8.png";
+export { default as Class9 } from "./class9.png";
+
+export { default as Img1 } from "./img1.jpeg";
+export { default as Img2 } from "./img2.jpeg";
+export { default as Img3 } from "./img3.jpeg";
+export { default as Img4 } from "./img4.jpeg";
+export { default as Img5 } from "./img5.jpeg";
+export { default as Img6 } from "./img6.jpeg";
+export { default as Img7 } from "./img7.jpeg";
+export { default as Img8 } from "./img8.jpeg";
+export { default as Img9 } from "./img9.jpeg";
+export { default as Img10 } from "./img10.jpeg";
